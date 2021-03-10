d8a4b7bd7ef0101ef5ef2f0557c295b12074e9a6
Benten559 <37288756+Benten559@users.noreply.github.com>
2021-03-09 19:26:55 -0800

    Commenting steps in the code.
    5 points method gives correct results on all test cases so far.

--- segScript.r
+++ segScript.r
@@ -1,10 +1,16 @@
 require("raster")
 library(base)
+
+### -- Prep for cluster process -- ###
 #take imagery from the two folders with different exposure
+# Notes: These img variables..(img_list, and img_list2) contain all exposures and frequencies of the same leaf.
+
+img_list<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_100050_ID980/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000',pattern = ".png", full.names = TRUE)[1:25]
+img_list2<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_100050_ID980/leaf_2020_1000ms_80/leaf_2020_1000ms_80_000000',pattern = ".png", full.names = TRUE)[26:80]
 
 #for ID 984
-img_list<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_094034_ID984/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000',pattern = ".png", full.names = TRUE)[1:25]
-img_list2<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_094034_ID984/leaf_2020_1000ms_80/leaf_2020_1000ms_80_000000',pattern = ".png", full.names = TRUE)[26:80]
+#img_list<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_094034_ID984/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000',pattern = ".png", full.names = TRUE)[1:25]
+#img_list2<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_094034_ID984/leaf_2020_1000ms_80/leaf_2020_1000ms_80_000000',pattern = ".png", full.names = TRUE)[26:80]
 # for ID 813
 #img_list <- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200828_110333_ID813/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000', pattern = ".png", full.names = TRUE)[1:25]
 #img_list2 <- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200828_110333_ID813/leaf_2020_1000ms_80/leaf_2020_1000ms_80_000000', pattern = ".png", full.names = TRUE)[26:80]
@@ -15,23 +21,27 @@ img_list2<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200
 #img_list<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200828_111217_ID812/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000',pattern = ".png", full.names = TRUE)[1:25]
 #img_list<- list.files('G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200828_111217_ID812/leaf_2020_1000ms_80/leaf_2020_1000ms_80_000000',pattern = ".png", full.names = TRUE)[1:25]
 
-#extract the reflectance values.
+## - extract the reflectance values - ##
 #TO DO: these are absolute values and should be replaced by calibrated ones
+
+#concatenate the list of exposures to one array.
 img_list <- c(img_list, img_list2)
-#cropping mask
+#cropping mask, this gets new view/size of current image dimensions
+# Notes: Our lab data goes from 128x128 -> 82x82 (from center)
 crop_percent <- 0.2
 crop_mask <- extent(crop_percent * 100, 128 * (1 - crop_percent),
                     crop_percent * 100, 128 * (1 - crop_percent))
 
+#Takes list of images and individually turn them into cropped bitmaps 
 img_values <- sapply(img_list, function(x) {
   img <- raster(x)
   img <- crop(img, crop_mask)
   as.data.frame(img)
 })
-
+# -- Find functional definition for this part --
 img_values <- do.call(rbind, img_values)
 img_values <- t(img_values)
-
+# - Produce the clustering object from applying Kmeans algorithm.
 img_df <- as.data.frame(img_values)
 k_img <- kmeans(img_df, 2)
 #k_img
@@ -42,10 +52,11 @@ k_img <- kmeans(img_df, 2)
 #str(clusters)
 #img_sub$size
 
+## -- Prep for semantic segmentation -- ##
+# Notes: this portion selects a single image, converts it to a bitmap then applys previous cluster info to create leaf class within data frame.
+img <-raster("G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_100050_ID980/leaf_2020_1000ms_80/leaf_2020_1000ms_80_000000/641.8_leaf_2020_1000ms_80_200919_100123_258.png")
 #for ID 984
-img <-raster("G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_094034_ID984/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000/500.0_leaf_2020_200ms_80wl_200919_094034_378.png")
-
-
+#img <-raster("G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_094034_ID984/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000/500.0_leaf_2020_200ms_80wl_200919_094034_378.png")
 #for ID 812
 #img <-raster("G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200828_111217_ID812/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000/500.0_leaf_2020_200ms_80wl_200828_111217_145.png")
 #for ID 810
@@ -53,6 +64,7 @@ img <-raster("G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200919_094034
 #for ID 813
 #img <- raster("G:/Shared drives/Leaf_cabinet_data/leaf_2020_cabinet_200828_110333_ID813/leaf_2020_200ms_80wl/leaf_2020_200ms_80wl_000000/540.5_leaf_2020_200ms_80wl_200828_110428_859.png")
 
+#Adds additional columns to image DF from x, y coord values, the z designates the clustering group ie..(1 or 2)
 img <- crop(img, crop_mask)
 img_df$x <- coordinates(img)[,1]
 img_df$y <- coordinates(img)[,2]
@@ -61,10 +73,10 @@ img_df$z <- k_img$cluster
 # -- Consider a test box to classify the leaf
 # our image is of dimension 82x82 flattened
 # linearIndex = width*x+y  
-# Note: Pixel values still seem offset, instead create mini DF if creates problem
-# the list of flattened testing points
+# Note: Pixel values still seem slightly offset, instead create mini DF if creates classification problem
+# bP <- the list of flattened testing points
 bP <- c(3403,3162,3654,3152,3644)
-#In order of C, TL, TR, BL, BR
+#For the points C, TL, TR, BL, BR
 k_img$cluster[3403]
 k_img$cluster[3162]
 k_img$cluster[3654]
@@ -81,8 +93,10 @@ assignLeaf <- function(img,boxPoints){
       count1 <- count1+1
     }
   }
-  str(count1)
+  #str(count1)
+  #if points had more occurence in cluster #1
   if(count1 >= 3)return(1)
+  # otherwise return the other cluster #2
   else return(2)
 }
 leaf <- assignLeaf(img_df$z,bP)
@@ -101,6 +115,7 @@ img_sub <- img_df[img_df$z == leaf,]
 img_sub <- kmeans(img_sub[1:82], 2)#changed originally from 1:82
 img_df[img_df$z == 1, "z"] <- 3
 img_df[img_df$z == 2, "z"] <- NA
+#this line won't throws error when Leaf is in cluster 1
 img_df[is.na(img_df$z), "z"] <- img_sub$cluster
 
 img_plot <- rasterFromXYZ(img_df[81:83])
